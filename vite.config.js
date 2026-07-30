8de81d55b81b8bcbb7dfdf2b47d559bdaece20aa
Yogesh Saini <yogeshsaini7172@gmail.com>
2026-07-30 21:44:54 +0530

    pwa implementation

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SISTec Digital Pass',
+        short_name: 'SISTec Digital Pass',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/digitalPassWeb/',
   server: {
     proxy: {
